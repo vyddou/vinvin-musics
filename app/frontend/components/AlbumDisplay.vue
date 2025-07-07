12ea1942c4c966346a68daec0c60641b6f3ad3ff
ugo <ugo.vidberg@gmail.com>
2025-07-07 11:26:24 +0200

    appel de l'api rails , récupération des données json

--- app/frontend/components/AlbumDisplay.vue
+++ app/frontend/components/AlbumDisplay.vue
@@ -10,14 +10,17 @@ onMounted(async () => {
   // éxécute le code de manière asynchrone que le composant est visible
   try {
     const response = await fetch('/api/v1/albums'); // appelle l'api et attends jusqu'a sa récupération
-    const data = await response.json
+    if (!response.ok) { // on vérifie que  la réponse du server soit dans les 200 et non 400 ou 500
+      throw new Error ("La réponse du réseau n'était pas ok"); //Si la réponse n'est pas bonne, on arrête immédiatement le bloc try et on "jette" une erreur. Le programme va sauter directement au bloc catch
+    }
 
+    const data = await response.json(); // si bonne réponse on la tranforme en obj js
     album.value = data[0]; // insére le premier album dans un array
 
   } catch (e) {
     // en cas d'erreur on la stock dans une variable
     error.value = "Impossible de charger l'album";
-    console.error(e);
+    console.error(e); // on affiche l'erreur technique complete dans la console
   } finally {
     // fin de chargement quelque soit le résultat
     loading.value = false;
