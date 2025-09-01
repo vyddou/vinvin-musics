e196ad70e646d17f4ae036a3d88576e4ce7ec74e
Ugo Vidberg <ugo.vidberg@gmail.com>
2025-09-01 15:18:26 +0200

    Add AlbumDisplay.vue to consume API and display album Simone

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,10 +1,4 @@
-import { createApp } from 'vue';
-// Importation du composant AlbumDisplay
-import AlbumDisplay from '../components/AlbumDisplay.vue';
+import { createApp } from 'vue'
+import AlbumDisplay from '../components/AlbumDisplay.vue'
 
-// Attend que le DOM soit chargé avant de lancer l'app Vue
-document.addEventListener('DOMContentLoaded', () => {
-  // Composant pour créer l'application Vue
-const app = createApp(AlbumDisplay);
-  app.mount('#app');
-});
+createApp(AlbumDisplay).mount('#app')
