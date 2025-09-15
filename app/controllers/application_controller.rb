391b3111ee9824163b50c23ffd517f05434599f3
Ugo Vidberg <ugo.vidberg@gmail.com>
2025-09-15 11:05:35 +0200

    feat(auth): filtre require_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
-end
+  # -- Devise: redirige vers login si non connecté
+  before_action :authenticate_user!
+
+  private
+
+  # -- Autorise seulement les admins
+  def require_admin!
+    # current_user est fourni par Devise
+    unless current_user&.admin?
+      # message + redirection si pas admin
+      redirect_to root_path, alert: "Accès réservé à l’admin."
+    end
+  end
+end 
